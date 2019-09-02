03b473d6955a330958743ac294b5a05b321b1d0b
Joseph Potechin <josephpotechin@gmail.com>
2019-09-02 07:55:53 -0700

    Update _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,9 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+
+// Ensurse Server rendering of Styled Components
+
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
