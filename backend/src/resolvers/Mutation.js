9281be15d870d814f0d0942e33b5550ce2f34921
Joseph Potechin <josephpotechin@gmail.com>
2019-09-03 06:48:06 -0700

    Update item feature was created

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,21 +1,36 @@
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-      // TODO: Check if they are logged in
-  
-      const item = await ctx.db.mutation.createItem(
-        {
-          data: {
-            ...args,
-          },
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
         },
-        info
-      );
-  
-      console.log(item);
-  
-      return item;
-    },
-  };
-  
-  module.exports = Mutations;
-  
+      },
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first get a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+};
+
+module.exports = Mutations;
