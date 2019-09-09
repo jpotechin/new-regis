a2fed8fd1ca8817bdc9f5712fe2691f4e2728da8
Joseph Potechin <josephpotechin@gmail.com>
2019-09-09 10:40:58 -0700

    Cart Count feature implemented

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,7 +3,8 @@ import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
-import Signout from './Signout'
+import CartCount from './CartCount';
+import Signout from './Signout';
 
 
 const Nav = () => (
@@ -27,7 +28,10 @@ const Nav = () => (
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
               {(toggleCart) => (
-                <button onClick={toggleCart}>My Cart</button>
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}></CartCount>
+                </button>
               )}
             </Mutation>
             
