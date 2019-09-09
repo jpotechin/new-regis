1f4572e5c4cb4bb217b307edcf23eaa4dae70791
Joseph Potechin <josephpotechin@gmail.com>
2019-09-09 11:39:22 -0700

    Client side Payment via Stripe

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -10,6 +10,7 @@ import RegisButton from './styles/RegisButton';
 import CartItem from './CartItem';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import formatMoney from '../lib/formatMoney';
+import TakeMyMoney from './TakeMyMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -22,13 +23,13 @@ const TOGGLE_CART_MUTATION = gql`
     toggleCart @client
   }
 `;
-
+/* eslint-disable */
 const Composed = adopt({
   user: ({ render }) => <User>{render}</User>,
   toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
   localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
 });
-
+/* eslint-enable */
 
 const Cart = () => (
   <Composed>
@@ -49,7 +50,9 @@ const Cart = () => (
           <ul>{me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}</ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <RegisButton>Checkout</RegisButton>
+            <TakeMyMoney>
+              <RegisButton>Checkout</RegisButton>
+            </TakeMyMoney>
           </footer>
         </CartStyles>
       );
