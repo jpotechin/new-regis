07abca402fd3038d6bf8c56c01c039c9875ab724
Joseph Potechin <josephpotechin@gmail.com>
2019-09-09 20:44:12 -0700

    Fixes

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -44,7 +44,7 @@ class CreateItem extends Component {
     const files = e.target.files;
     const data = new FormData();
     data.append('file', files[0]);
-    data.append('upload_preset', 'sickfits');
+    data.append('upload_preset', 'authenticregis');
 
     const res = await fetch('https://api.cloudinary.com/v1_1/vlazlo/image/upload', {
       method: 'POST',
